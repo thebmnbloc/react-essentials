db5206ff912304b49181fce89c5e33f51ad233c5
thebmnbloc <bioralak46@gmail.com>
2026-05-18 13:23:07 +0200

    add updates to dynamic structures

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
           <h2 className="flex items-center justify-center text-white text-2xl">Core Concepts</h2>
           <ul className="flex items-center justify-center bg-slate-800 px-6 py-6 gap-2 rounded-lg">
             {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept {...conceptItem}  />
+              <CoreConcept key={conceptItem.title} {...conceptItem}  />
             ))}
           </ul>
         </section>
--- src/components/TabButton.tsx
+++ src/components/TabButton.tsx
@@ -1,7 +1,7 @@
 export default function TabButton({children, onSelect, isSelected}) {
   return <button 
   onClick={onSelect}
-  className={`px-6 py-3 hover:bg-slate-800 rounded-lg ${isSelected && 'bg-blue-400'}`}
+  className={`px-6 py-3 hover:bg-slate-800 rounded-lg ${isSelected && 'bg-white'}`}
   >
     {children}
   </button>
